d9068cba0250a8b7677b923b3b883aa793e0cca4
irvinalcira <irvinalcira@gmail.com>
2019-10-12 00:03:01 -0700

    Initial Files

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import styles from './styles/AppStyles';
+
+function App(){
+  return (
+
+      <SafeAreaView>
+           <Text>Ryde</Text>
+      </SafeAreaView>
+
+  )
+};
+
+export default App;
