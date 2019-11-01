e64e87ce12fc2cb98c0709247e710febf84b935a
irvinalcira <irvinalcira@gmail.com>
2019-10-31 19:24:48 -0700

    Renamed MainStyles

--- styles/mainstyles.js
+++ styles/mainstyles.js
@@ -1,11 +1,11 @@
 import {StyleSheet} from 'react-native';
 
-var styles=StyleSheet.create({
-    main:{
+var MainStyles = StyleSheet.create({
+    Main:{
         flex:1,
         justifyContent:"center",
         alignItems:"center",
     }
 })
 
-export default styles;
+export default MainStyles;
