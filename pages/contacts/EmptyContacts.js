de14f0b2b1804db52c755f8be76103fb4cae799d
irvinalcira <irvinalcira@gmail.com>
2019-11-15 15:09:00 -0800

    EmptyContacts is now a component
    what title says

--- pages/contacts/EmptyContacts.js
+++ pages/contacts/EmptyContacts.js
@@ -9,25 +9,21 @@ import EContactsStyles from '../../styles/contacts/EmptyContactsStyles';
 
 function EmptyContacts(){
   return (
-    <SafeAreaView style={EContactsStyles.Container}>
-      <View style={EContactsStyles.Container}>
-        <Text style={Fonts.Title}>Contacts</Text>
-
-        <Text style={Fonts.Heading}>You have no Contacts on Ryde</Text>
+    <View>
+        <Text style={[Fonts.Heading, {marginTop:0}]}>You have no Contacts on Ryde</Text>
         <Text style={Fonts.Body}>
         Start by adding your important contacts for quick phone or messaging access. You can edit, add, or remove contacts anytime.
         </Text>
-        
+
         <TouchableOpacity  style={[Buttons.Main, {marginTop:35}]} onPress={() => Actions.AddContact()}>
-       <Text style={Buttons.MainText}>Import Contacts from Phone</Text>
+        <Text style={Buttons.MainText}>Import Contacts from Phone</Text>
         </TouchableOpacity>
 
         <TouchableOpacity  style={Buttons.Alt} onPress={() => Actions.AddContact()}>
-       <Text style={Buttons.MainText}>Manually Add Contacts</Text>
+        <Text style={Buttons.MainText}>Manually Add Contacts</Text>
         </TouchableOpacity>
-
-      </View>
-      </SafeAreaView>
+    </View>
+        
   )
 };
 
