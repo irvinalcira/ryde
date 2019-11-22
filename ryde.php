59aff0ad94071a24cfbf12a28ef82be3287410ee
abhimannyucc <abhimannyucc@gmail.com>
2019-11-22 13:02:34 -0800

    done

--- ryde.php
+++ ryde.php
@@ -4,6 +4,11 @@ $server = $url["us-cdbr-iron-east-05.cleardb.net"];
 $username = $url["bd647197a172ad"];
 $password = $url["a5f7622f"];
 $dbname = substr($url["heroku_8995ffa5c160baf"], 1);
+var_dump($url);
+var_dump($server);
+var_dump($username);
+var_dump($password);
+var_dump($dbname);
 
 $conn = new mysqli($server, $username, $password, $dbname);
 mysqli_select_db($dbname);
