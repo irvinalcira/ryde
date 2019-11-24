b390ddaa6116eb34e2882bf61996078bd62b7311
Shenis Osias <shenisosias@gmail.com>
2019-11-24 12:28:45 -0800

    Problem with loading json file

--- pages/Loading.js
+++ pages/Loading.js
@@ -6,9 +6,11 @@ import LottieView from 'lottie-react-native';
 function Loading() {
   return (
     <SafeAreaView style={{justifyContent: "center", alignItems: "center"}}>
-      {/* <LottieView source={require('../assets/animations/117-progress-bar.json')} autoPlay loop /> */}
-      <Image
-      source={require('../assets/animations/117-progress-bar.json')} />
+      <LottieView source={require('../assets/animations/117-progress-bar.json')} autoPlay loop />
+      <Image 
+      style={{width: 200, height: 200}}
+      source={require('../assets/animations/117-progress-bar.gif')} />
+       {/* <Image source={require('../assets/animation/222-trail-loading.gif')} /> */}
     </SafeAreaView>
   )
 }
