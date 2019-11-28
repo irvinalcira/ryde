a8f89e861118b0b657b92b58624ebf4ce973b79e
irvinalcira <irvinalcira@gmail.com>
2019-11-27 23:10:34 -0800

    Fixed Styles

--- styles/home/HomePageStyles.js
+++ styles/home/HomePageStyles.js
@@ -6,8 +6,7 @@ var HomePageStyles = StyleSheet.create({
     flex:1,
     padding:15,
     marginTop:20,
-    backgroundColor:'#eee'
-    
+    // backgroundColor:'lightblue'
   },
 
   FavContainer:{
@@ -63,8 +62,16 @@ var HomePageStyles = StyleSheet.create({
     margin: 10,
   },
 
+  ProfPic: {
+    width:'100%',
+    height:'100%',
+    borderRadius:150,
+    
+  },
+
+
   ContactList:{
-    flexDirection: 'row',
+    flexDirection:'row',
     alignItems:'center',
     justifyContent:'center',
   },
