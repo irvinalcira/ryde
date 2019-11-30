19e99820139759ba87c60f0e67bcc135a1ae5da1
bin <quadclipse@gmail.com>
2019-11-29 22:26:07 -0800

    Bus Works

--- pages/bus/FullBusSchedule.js
+++ pages/bus/FullBusSchedule.js
@@ -1,15 +1,90 @@
-import React, { useState } from 'react';
-import {View,Text, Image,SafeAreaView, TouchableOpacity} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {View,Text, Image,SafeAreaView, TouchableOpacity, AsyncStorage} from 'react-native';
 import {Actions} from 'react-native-router-flux';
+import data from '../../storage.json';
+import Communications from 'react-native-communications';
 
 import FullBusStyles from '../../styles/bus/FullBusScheduleStyles';
 import Fonts from '../../styles/FontsStyles';
+import Buttons from '../../styles/ButtonsStyles';
 
 import Divider from '../../comps/Divider';
 
 function FullBusSchedule({RouteNo,RouteName,Schedules,StopNumber}) {
 // console.log(Schedules[0])
 let Min;
+
+  var notFavorited = require('../../assets/icons/favorite2.png');
+  var busFavorited = require('../../assets/icons/favorite.png');
+
+  const [ favBus, setFavBus ] = useState([]);
+  const [ BusFaved, SetBusFaved ] = useState(false);
+  const [ FavArr, SetFavArr ] = useState([]);
+  const [ favBusImg, setFavBusImg ] = useState(notFavorited);
+
+  async function UpdateFavBus(){
+
+    var datanew = await AsyncStorage.getItem("storage");
+    if(!datanew){
+      datanew = data;
+    } else {
+      datanew = JSON.parse(datanew)
+    }
+      if (BusFaved === false){
+        datanew.FavBus.push({
+          favbusrouteno: RouteNo,
+          favbusstopno: StopNumber,
+          favbusroutename: RouteName,
+          favbustime: Schedules[0].ExpectedCountdown,
+        })
+        SetFavArr(datanew.FavBus);
+        SetBusFaved(true);
+        setFavBusImg(busFavorited);
+      } else {
+        DeleteBusFav()
+      }
+      console.log("test",datanew.FavBus);
+      AsyncStorage.setItem("storage",JSON.stringify(datanew));
+  };
+
+  async function DeleteBusFav(){
+    var data = await AsyncStorage.getItem("storage");
+    parsedelete = JSON.parse(data);
+    var favBusFilter = parsedelete.FavBus.filter((o,i)=>{
+      return o.favbusrouteno !== RouteNo||o.favbusstopno !== StopNumber;
+    });
+    parsedelete.FavBus = favBusFilter;
+    await AsyncStorage.setItem("storage", JSON.stringify(parsedelete));
+    SetBusFaved(false);
+    setFavBusImg(notFavorited);
+    console.log(favBusFilter);
+  }
+  
+  async function CheckBusColor(){
+    var datanew = await AsyncStorage.getItem("storage");
+    if (!datanew){
+      setFavBusImg(notFavorited);
+    } else {
+      datamew = JSON.parse(datanew)
+    }
+    var favBusFilter = datanew.FavBus.filter((o,i)=>{
+      return o.favbusrouteno !== RouteNo||o.favbusstopno !== StopNumber;
+    });
+    if(favBusFilter.length>0){
+      setFavBusImg(busFavorited)
+      SetBusFaved(true);
+    } else {
+      setFavBusImg(notFavorited)
+      SetBusFaved(false);
+    }
+  }
+
+  useEffect(() => {
+
+    CheckBusColor();
+  }, []);
+
+
   return (
     <SafeAreaView style={FullBusStyles.Container}>
       <View style={FullBusStyles.Container}> 
@@ -31,7 +106,17 @@ let Min;
 
               {/* Stop Number */}
                <Text style={[Fonts.BusRoute, FullBusStyles.NavTitle]}>{StopNumber}</Text>
+               <TouchableOpacity style={{justifyContent:'center', alignItems:'center', flex:1, flexDirection:'row-reverse'}}
+              onPress={ async() => {
+                UpdateFavBus()
+                console.log(favBus);
+              }}>
+
+                <Image style={{height:30, width:30}} source={favBusImg} />
+
+            </TouchableOpacity>
             </View>
+
              
             <View style={FullBusStyles.MidStyles}>
               <View style={FullBusStyles.BusStopInfo}>
